574fcdc1dc888465e25681b8f562b2d391e32b5a
Roreo <rory.tomkins@education.nsw.gov.au>
2026-05-14 11:10:37 +1000

    q

--- Data_Module.py
+++ Data_Module.py
@@ -1,3 +1,15 @@
+import pandas as pd
+import os, time
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def total_data():
+    total_df = pd.read_csv('Data Science Project.csv')
+    print(total_df)
+    print('')
+    rm = input('Press ENTER to return to menu')
 def visualise_data():
     pass
 
--- Main.py
+++ Main.py
@@ -1,35 +1,36 @@
 import os, time
 
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+import pandas as pd
 
+from Data_Module import total_data, clear_screen
 
 def menu():
     while True:
-        print('╔══════════════════════════════════╗ \n║           Data Science           ║\n╠══════════════════════════════════╣\n║ 1 > Veiw Visualisation           ║\n║ 2 > Search/Filter Data           ║\n║ 3 > Veiw Mean Data               ║\n║ 4 > Update data                  ║\n║ 5 > Quit                         ║\n╠══════════════════════════════════╣\n║  Enter Option (1-5) to continue  ║\n╚══════════════════════════════════╝')
+        print('╔══════════════════════════════════╗ \n║           Data Science           ║\n╠══════════════════════════════════╣\n║ 1 > Veiw Data                    ║\n║ 2 > Veiw Visualisation           ║\n║ 3 > Search/Filter Data           ║\n║ 4 > Veiw Mean Data               ║\n║ 5 > Update data                  ║\n║ 6 > Quit                         ║\n╠══════════════════════════════════╣\n║  Enter Option (1-6) to continue  ║\n╚══════════════════════════════════╝')
 
-        option = input()
+        option = int(input("Input:"))
 
-        if option == '1':
+        if option == 1:
+            total_data()
+        elif option == 2:
             #visualise_data()
             pass
-        elif option == '2':
+        elif option == 3:
             #search_data()
             pass
-        elif option == '3':
+        elif option == 4:
             #mean_data()
             pass
-        elif option == '4':
+        elif option == 5:
             #update_data()
             pass
-        elif option == '5':
+        elif option == 6:
             #save_changes()
             print("Quiting Program...")
             break
         else:
-            print("Invalid selection. Please choose a number between 1 and 5.")
-    time.sleep(2)
-    clear_screen()
+            print("Invalid selection. Please choose a number between 1 and 6.")
+        clear_screen()
 
 if __name__ == "__main__":
     menu()
